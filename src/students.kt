517f7865c586a278ae29f6a6c0623fbd0ebda7af
benjamin <benjamin.martinez.pro@gmail.com>
2019-02-03 22:23:27 +0100

    added students case tests

---
+++ src/students.kt
@@ -0,0 +1,11 @@
+data class Student(val firstName: String, val lastName: String, val house: String, val year: Int = 1)
+fun main(args: Array<String>)
+{    val s1 = Student("Ron", "Weasley", "Gryffindor")
+    val s2 = Student("Draco", "Malfoy", house = "Slytherin")
+    val s3 = s1.copy(firstName = "Fred", year = 3)
+    val s4 = s3.copy(firstName = "George")
+    val array = arrayOf(s1, s2, s3, s4)
+    for ((vFirstName, vLastName, vHouse, vYear) in array) {
+        println("$vFirstName $vLastName is in $vHouse year $vYear")
+    }
+}
