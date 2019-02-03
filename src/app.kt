c0d14ab5ebffb4251a827e5006f8f3aa1db03447
benjamin <benjamin.martinez.pro@gmail.com>
2019-02-03 22:12:41 +0100

    first commit

---
+++ src/app.kt
@@ -0,0 +1,49 @@
+data class Recipe(val title : String,
+    val mainIngredient: String,
+    val isVegetarian: Boolean = false,
+    val difficulty : String = "Easy") {
+
+    class Mushroom(val size : Int, val isMagic : Boolean) {
+        constructor(isMagic_Param : Boolean) : this(0, isMagic_Param) {
+            //Code that runs when the secondary constructor is called
+        }
+    }
+
+    fun findRecipes(title : String = "",
+                   ingredient : String = "",
+                   isVegetarian : Boolean = false,
+                   difficulty: String = "") : Array<Recipe> {
+        //code to find recipes
+        return arrayOf(Recipe(title, ingredient, isVegetarian,difficulty))
+    }
+
+    fun addNumbers(a: Int, b: Int) : Int{
+        return a + b
+    }
+    fun addNumbers(a: Double, b: Double) : Double
+    {
+        return a + b
+    }
+
+    fun main (args : Array<String>) {
+
+        val r1 = Recipe("Thai Curry", "Chicken")
+        val r2 = Recipe(title = "Thai Curry", mainIngredient = "Chicken")
+        val r3 = r1.copy(title = "Chicken Bhuna")
+
+        println("r1 hasjh code : ${r1.hashCode()}")
+        println("r2 hasjh code : ${r2.hashCode()}")
+        println("r3 hasjh code : ${r3.hashCode()}")
+
+        val(vTitle, vMainIngredient,vIsvegetarian, vDifficulty) = r1
+        println("vTitle is $vTitle and visVegetarian is $vIsvegetarian")
+
+        val m1 = Mushroom(6, false)
+        println("m1 size is ${m1.size} and isMagic is ${m1.isMagic}")
+        val m2 = Mushroom(true)
+        println("m1 size is ${m2.size} and isMagic is ${m2.isMagic}")
+
+        println(addNumbers(2,5))
+        println(addNumbers(2.1, 5.0))
+    }
+}
